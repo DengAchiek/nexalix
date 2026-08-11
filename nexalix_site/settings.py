923b318f1c151f9b0ebca0c5348f24ffc9215054
ChinksCodeLab CCL <109124956+DengAchiek@users.noreply.github.com>
2026-08-11 11:54:58 +0300

    Refactor database settings based on DATABASE_URL
    Refactor database configuration to use DATABASE_URL for production and fallback to SQLite for development.

--- nexalix_site/settings.py
+++ nexalix_site/settings.py
@@ -83,20 +83,23 @@ WSGI_APPLICATION = "nexalix_site.wsgi.application"
 
 # DATABASE
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 DATABASE_URL = os.getenv("DATABASE_URL")
-if DATABASE_URL and dj_database_url:
-    DATABASES["default"] = dj_database_url.parse(
-        DATABASE_URL,
-        conn_max_age=600,
-        ssl_require=not DEBUG,
-    )
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600,
+            ssl_require=not DEBUG,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # CACHE (Redis if available)
